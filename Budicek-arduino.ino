2633555e5a28ce2f5d0caad8ce7d7e0106557d38
xcidip <xcidip@tutanota.com>
2024-04-08 08:31:57 +0200

    GetInput function

--- Budicek-arduino.ino
+++ Budicek-arduino.ino
@@ -2,15 +2,15 @@ int btn1 = 9;
 int btn2 = 8;
 bool isIdle = false;
 
+int choice = 0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(btn1, INPUT);  // Set button 1 pin as input
   pinMode(btn2, INPUT);  // Set button 2 pin as input
-  
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
+int GetInput(){
   int val1 = digitalRead(btn1);
   int val2 = digitalRead(btn2);
   int val3 = 1;
@@ -37,16 +37,36 @@ void loop() {
 
   if (val1 == 0){
     Serial.println("button 1 is active");
+    return 1;
   }
   else if (val2 == 0){
     Serial.println("button 2 is active");
+    return 2;
   }
   else if (val3 == 0){
     Serial.println("button 3 is active");
+    return 3;
   }
   else if (val4 == 0){
     Serial.println("button 4 is active");
+    return 4;
+  }
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  choice = GetInput();
+  delay(300);  // delay in between reads for stability
+  
+  switch (choice){
+    case 1:
+      // change mode
+    case 2:
+      // confirm choice
+    case 3:
+      // time up
+    case 4:
+      // time down
   }
 
-  delay(300);  // delay in between reads for stability
 }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # budicek-arduino
+---
+- při zapnutí
+  - prompt pro nastavení času
+  - prompt pro nastavení kdy budík zazvoní
+- default mod
+  - Aktuální čas:
+    22:20
+- mod nastaveni budiku
+  - Čas budíku:
+    12:20
+---
+## Loop
+- check na input
+- check jestli se čas budíku rovná času aktuálním
+--
+## Tlačítka 
+1. změna modu
+2. potvrzení své volby
+3. čas nahoru
+4. čas dolu
+---
+## GetInput()
+- získá input a vrací 1-4
